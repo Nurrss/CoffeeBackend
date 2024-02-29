0b91b40dd1bea0cd26077feb43c9531a9eb14d71
Nursultan <nurrs.serkul@gmail.com>
2024-02-29 19:19:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const port = 8000;
+
+app.use(express.json());
+app.use(morgan("common"));
+
+const DB_URL =
+  "mongodb+srv://nurrsserkul:1234@cluster0.cvoja0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(port, () => {
+    console.log("Backend server is running at: ", port);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
+
+// it should be in the end
+app.use(function (req, res) {
+  return res.status(404).json({ message: "Endpoint not found" });
+});
