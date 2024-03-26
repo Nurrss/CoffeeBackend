15629524f4c54e2f05f6cdccd1adaac6fb70aa9a
Azhara <azhara@MBP-Azhara.lan>
2024-03-26 20:27:32 +0500

    swagger was updated

--- routes/order.js
+++ routes/order.js
@@ -12,10 +12,136 @@ const errorHandler = (err, req, res) => {
   res.status(500).json({ error: "Internal Server Error" });
 };
 
-router.get("/", getAllOrders);
-router.get("/:id", getOrderById);
-router.delete("/:id", deleteOrderById);
+/**
+ *
+ * @swagger
+ * tags:
+ *   name: Order
+ *   description: Order manipulating routes.
+ */
+
+/**
+ * @swagger
+ * /order/add:
+ *   post:
+ *     summary: Add a new item
+ *     description: Endpoint to add a new item with name and price.
+ *     tags: [Order]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               aksha:
+ *                 type: string
+ *                 description: The name of the item.
+ *     responses:
+ *       '201':
+ *         description: Item created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 aksha:
+ *                   type: string
+ *                   description: The price of the created item.
+ *       '400':
+ *         description: Bad request. Missing or invalid parameters.
+ *       '500':
+ *         description: Internal server error.
+ */
+
 router.post("/add", addOrder);
+
+/**
+ * @swagger
+ * /order:
+ *   get:
+ *     summary: Get all items
+ *     description: Endpoint to retrieve all items.
+ *     tags: [Order]
+ *     responses:
+ *       '200':
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/definitions/Order'
+ *       '500':
+ *         description: Internal server error.
+ */
+
+router.get("/", getAllOrders);
+
+/**
+ * @swagger
+ * /order/{id}:
+ *   get:
+ *     summary: Get item by ID
+ *     description: Endpoint to retrieve an item by its ID.
+ *     tags: [Order]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the item to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Order'
+ *       '404':
+ *         description: Order not found
+ *       '500':
+ *         description: Internal server error.
+ */
+router.get("/:id", getOrderById);
+
+/**
+ * @swagger
+ * /order/update/{id}:
+ *   put:
+ *     summary: Update item by ID
+ *     description: Endpoint to update an item by its ID.
+ *     tags: [Order]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the item to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               aksha:
+ *                 type: string
+ *                 description: The name of the item.
+ *     responses:
+ *       '200':
+ *         description: Item updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Order'
+ *       '404':
+ *         description: Order not found
+ *       '500':
+ *         description: Internal server error.
+ */
 
 // router.post("/", async (req, res) => {
 //   try {
@@ -48,6 +174,7 @@ router.post("/add", addOrder);
 //     errorHandler(err, req, res);
 //   }
 // });
+router.delete("/:id", deleteOrderById);
 
 // // router.put("/:id", async (req, res) => {
 // //   try {
